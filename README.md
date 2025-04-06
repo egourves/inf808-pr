c8f50a1d9265e47eed27d0bb9957c5c77f3e2921
egourves <ewen@gourves.net>
2025-04-06 14:22:07 -0400

    feat: add powershell output to script

--- README.md
+++ README.md
@@ -29,3 +29,29 @@ podman run --rm -v $PWD:/app:z ubuntu ./app/fix.sh
 ## Compilation LaTeX
 
 Dans le dossier `.github` se trouve un fichier de configuration pour lancer des Github Actions qui permettent de compiler les fichiers LaTeX lorsque ceux-ci sont modifiés sur la branche `main`
+
+## Scripts Powershell
+
+Le scrip`Get-TTP.ps1` permet d'obtenir les TTP générées dans les Windows Event par Aurora.
+
+Pour obtenir les TTP les plus communes (nombre d'occurence), on peut utiliser la commande suivante :
+```powershell
+. Get-TTP.ps1
+' | Group-Object -Property TPP | ForEach-Object {     [PSCustomObject]@{
+>>         TTP = $_.Name  # Reuse the category from the original objects
+>>         Count = $_.Count
+>>         EventDetail = $_.Group  # Retain the original objects in the group
+>>     }} | Sort-Object -Property Count
+```
+
+Un exemple de sortie du script avec la commande précédente :
+```powershell
+T1027.004     1 {@{TPP=T1027.004; EventTime=2025-04-05T23:38:37.487153400Z; Level=2; ParentCommandLine= powershell; CommandLine=\Users\A…
+T1047         1 {@{TPP=T1047; EventTime=2025-04-05T23:17:51.812186200Z; Level=2; ParentCommandLine="127.0.0.1" product where "name like …"
+T1059.001     2 {@{TPP=T1059.001; EventTime=2025-04-06T00:45:33.106045900Z; Level=2; ParentCommandLine=FromBase64String('CiR1dWlkPSI1ZDk…'
+T1562.004     2 {@{TPP=T1562.004; EventTime=2025-04-05T23:09:57.467157400Z; Level=2; ParentCommandLine=; CommandLine=; EventID=99}, @{TP…
+T1053.005     7 {@{TPP=T1053.005; EventTime=2025-04-05T23:38:51.527068600Z; Level=2; ParentCommandLine=; CommandLine=; EventID=99}, @{TP…
+T1560.001    18 {@{TPP=T1560.001; EventTime=2025-04-06T02:24:49.288111200Z; Level=3; ParentCommandLine=//ts-caldera.tailce1ce1.ts.net -g…
+T1087.002    27 {@{TPP=T1087.002; EventTime=2025-04-06T02:26:21.033853800Z; Level=3; ParentCommandLine= powershell.exe -ExecutionPolicy …
+T1219       224 {@{TPP=T1219; EventTime=2025-04-06T02:20:57.869721300Z; Level=2; ParentCommandLine=\Users\Administrator\Desktop\AnyDesk.…
+```
--- scripts/Get-TTP.ps1
+++ scripts/Get-TTP.ps1
@@ -1,21 +1,33 @@
-# Read script arguments (output file)
+# Read script arguments
 param (
+    [string]$outputToAFile = $false,
     [string]$outputFile = "TTPs.csv"
 )
 
-# Check if the output file already exists and ask for confirmation to overwrite
-if (Test-Path $outputFile) {
-    $overwrite = Read-Host "The file $outputFile already exists. Do you want to overwrite it? (Y/N)"
-    if ($overwrite -ne 'Y') {
-        Write-Host "Exiting script without overwriting the file." -ForegroundColor Yellow
-        exit
-    } else {
-        Remove-Item $outputFile -Force
+# Dictionary to match level names to their corresponding numbers
+$levelDictionary = @{
+    "low" = 1
+    "medium" = 2
+    "high" = 3
+    "critical" = 4
+}
+
+# check if the output file already exists and ask for confirmation to overwrite
+if ($outputToAFile -eq $true) {
+    if (Test-Path $outputFile) {
+        $overwrite = Read-Host "The file $outputFile already exists. Do you want to overwrite it? (Y/N)"
+        if ($overwrite -ne 'Y') {
+            Write-Host "Exiting script without overwriting the file." -ForegroundColor Yellow
+            exit
+        } else {
+            Remove-Item $outputFile -Force
+        }
     }
 }
 
+# Define the regex pattern for TTP numbers (TXXXX or TXXXX.XX)
+$pattern = "T\d{4}(\.\d{3})?"
 # Query events from the "Application" log for the "AuroraAgent" provider and format as XML
-$pattern = "T\d{4}"
 $Events = Get-WinEvent -LogName "Application" -FilterXPath "*[System[(Provider[@Name='AuroraAgent'])]]"
 
 # Initialize variables for progress tracking
@@ -42,11 +54,18 @@ $Events | ForEach-Object {
             $output = [PSCustomObject]@{
                 TPP = $matches[0]
                 EventTime = $eventXml.System.TimeCreated.SystemTime
+                Level = $levelDictionary[((($eventXml.EventData.Data | Where-Object { $_ -like "Rule_Level*" } | Select-Object -First 1) -split ':' | Select-Object -Last 1) -replace ' ', '')]
+                ParentCommandLine = ($eventXml.EventData.Data | Where-Object { $_ -like "ParentCommandLine*" } | Select-Object -First 1) -split ':' | Select-Object -Last 1
+                CommandLine = ($eventXml.EventData.Data | Where-Object { $_ -like "CommandLine*" } | Select-Object -First 1) -split ':' | Select-Object -Last 1
                 EventID = $eventXml.System.EventID.'#text'
-                RecordID = $eventXml.System.EventRecordID
             }
             # Write the output to a file
-            $output | Export-Csv -Path $outputFile -NoTypeInformation -Append
+            if($outputToAFile -eq $true) {
+                 $output | Export-Csv -Path $outputFile -NoTypeInformation -Append
+            } else {
+                # Store the output in a variable for later use
+                $output | Select-Object TPP, EventTime, Level, ParentCommandLine, CommandLine, EventID
+            }
         }
     }
 
@@ -57,4 +76,8 @@ $Events | ForEach-Object {
 
 # Completion message
 Write-Host "Processing complete." -ForegroundColor Green
-Write-Host "Results saved to TTPs.csv" -ForegroundColor Green
+if ($outputToAFile -eq $true) {
+    Write-Host "Results saved to TTPs.csv" -ForegroundColor Green
+} else {
+    return $output
+}
